d2184311f4a792f95976ddb0f6d6de5c9b033d63
Ruben Csepke <ruben.csepke@gmail.com>
2025-03-01 09:06:40 +0100

    feat: create products table with dynamodb

--- product_service/lambda/getProductsList.ts
+++ product_service/lambda/getProductsList.ts
@@ -1,10 +1,17 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { Product } from "../model/product";
+
 export const handler = async () => {
 
-    const products = [
-        { id: '1', title: 'Product 1', price: 100 },
-        { id: '2', title: 'Product 2', price: 200 },
-        { id: '3', title: 'Product 3', price: 300 },
-    ]
+    const client = new DynamoDBClient({ region: "eu-central-1" });
+    const dynamodb = DynamoDBDocumentClient.from(client);
+
+    const productsResult = await dynamodb.send(new ScanCommand({
+        TableName: "Products",
+    }));
+
+    const products: Product[] = productsResult.Items as unknown as Product[] || [];
 
     return {
         statusCode: 200,
--- product_service/lib/nodejs-aws-shop-react-backend-stack.ts
+++ product_service/lib/nodejs-aws-shop-react-backend-stack.ts
@@ -2,23 +2,38 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import path = require('path');
 
 export class NodejsAwsShopReactBackendStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
+		// Define DynamoDB table
+		const productsTable = new dynamodb.Table(this, 'ProductsTable', {
+			partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+			tableName: 'Products',
+			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+		});
+
 		//Define Lambda function resource
 		const getProductsListFunction = new lambda.Function(this, 'GetProductsListFunction', {
 			runtime: lambda.Runtime.NODEJS_20_X,
 			handler: 'getProductsList.handler',
 			code: lambda.Code.fromAsset('lambda'),
+			environment: {
+				PRODUCTS_TABLE_NAME: productsTable.tableName,
+			},
 		});
 
 		const getProductsByIdFunction = new lambda.Function(this, 'GetProductsByIdFunction', {
 			runtime: lambda.Runtime.NODEJS_20_X,
 			handler: 'getProductsById.handler',
 			code: lambda.Code.fromAsset('lambda'),
+			environment: {
+				PRODUCTS_TABLE_NAME: productsTable.tableName,
+			},
 		});
 
 		// Define API Gateway resource
@@ -30,6 +45,9 @@ export class NodejsAwsShopReactBackendStack extends cdk.Stack {
 				allowHeaders: ['Content-Type'],
             },
 		});
+
+		productsTable.grantReadData(getProductsListFunction);
+		productsTable.grantReadData(getProductsByIdFunction);
 
 		// Define lambda resources with GET method
 		const products = api.root.addResource('products');
