ee91ea074c0594390b03f5cf1d2154d2fdf0546f
Ruben Csepke <ruben.csepke@gmail.com>
2025-03-01 11:40:01 +0100

    feat: update getProductById lambda with dynamodb functions

--- product_service/lambda/getProductsById.ts
+++ product_service/lambda/getProductsById.ts
@@ -1,15 +1,29 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { Product, ProductWithStock } from "../model/product";
+import { Stock } from "../model/stock";
+
 export const handler = async (event: any) => {
 
-    const products = [
-        { id: '1', title: 'Product 1', price: 100 },
-        { id: '2', title: 'Product 2', price: 200 },
-        { id: '3', title: 'Product 3', price: 300 },
-    ]
-
+    const client = new DynamoDBClient({ region: "eu-central-1" });
+    const dynamodb = DynamoDBDocumentClient.from(client);
     const productId = event.pathParameters?.productId;
-    const product = products.find((product) => product.id === productId);
 
-    if (!product) {
+    const productsResult = await dynamodb.send(new GetCommand({
+        TableName: "Products",
+        Key: {
+            id: productId
+        }
+    }));
+
+    const stocksResult = await dynamodb.send(new GetCommand({
+        TableName: "Stocks",
+        Key: {
+            product_id: productId
+        }
+    }));
+
+    if (!productsResult.Item) {
         return {
             statusCode: 404,
             headers: {
@@ -21,6 +35,11 @@ export const handler = async (event: any) => {
             body: JSON.stringify({ message: 'Product not found' }),
         };
     }
+
+    const product: ProductWithStock = {
+        ...productsResult.Item as Product,
+        count: (stocksResult.Item as Stock)?.count || 0
+    };
 
     return {
         statusCode: 200,
